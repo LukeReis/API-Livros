db0338ba33c714092190a0111516c8a7514178fa
Lucas Reis <lucaswdex@gmail.com>
2023-04-04 12:06:55 -0300

    Corrigido leitura e edição de arquivo .json nos endpoints

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-# 1. Objetivo - Criar uma API que disponibiliza a consulta, criacao, edicao e exclusao de livros
-# 2. URL base - localhost
-# 3. Endpoints - 
-    # - localhost/livros (GET)
-    # - localhost/livros/id (GET)
-    # - localhost/livros/id (PUT)
-    # - localhost/livros (POST)
-    # - localhost/livros/id (DELETE)
-
-# 4. Quais os recursos - Livros
-
 
 from flask import Flask, jsonify, request
 import json
@@ -16,54 +5,86 @@ import json
 
 app = Flask(__name__)
 
-with open("livrojson.json", "r") as arquivo:
-    for linha in arquivo:
-        try:
-            livros = json.load(arquivo)
-        except json.decoder.JSONDecodeError:
-            print('Erro ao decodificar objeto JSON')
 
-# Consultar (todos)
-@app.route('/livros', methods=['GET'])
-def obter_livros():
-    with open ("livrojson.json", "r") as arquivo:
+def carregar_livros():
+    global arquivo
+    with open("livrojson.json", "r+") as arquivo:
+        global livros
         livros = json.load(arquivo)
     return jsonify(livros)
 
+# Consultar (todos)
+
+
+@app.route('/livros', methods=['GET'])
+def obter_livros():
+    carregar_livros()
+    return carregar_livros()
 
 # Consultar(id)
+
+
 @app.route('/livros/<int:id>', methods=['GET'])
 def obter_livro_por_id(id):
+    carregar_livros()
     for livro in livros:
         if livro.get('id') == id:
-            return jsonify(livros)
-
+            return jsonify(livro)
+    return jsonify({'erro': 'livro nao encontrado'})
 
 # Editar
+
+
 @app.route('/livros/<int:id>', methods=['PUT'])
 def editar_livro_por_id(id):
     livro_alterado = request.get_json()
+    carregar_livros()
+
+    livro_encontrado = False
     for indice, livro in enumerate(livros):
         if livro.get('id') == id:
             livros[indice].update(livro_alterado)
+            livro_encontrado = True
+            break
+
+    if livro_encontrado:
+        with open("livrojson.json", "w") as arquivo:
+            json.dump(livros, arquivo, indent=4)
             return jsonify(livros[indice])
-    
+    else:
+        return jsonify({'erro', 'livro não encontrado'})
+
 # Criar
+
+
 @app.route('/livros', methods=['POST'])
 def incluir_novo_livro():
     novo_livro = request.get_json()
+    carregar_livros()
+
+    novo_livro['id'] = len(livros) + 1
     livros.append(novo_livro)
+
+    with open("livrojson.json", "w") as arquivo:
+        json.dump(livros, arquivo, indent=4)
 
     return jsonify(livros)
 
 # Excluir
+
+
 @app.route('/livros/<int:id>', methods=['DELETE'])
 def excluir_livro(id):
-    for indice, livro in enumerate(livros):
-        if livro.get('id') == id:
-            del livros[indice]
-
-    return jsonify(livros)
+    carregar_livros()
+    for livro in livros:
+        if livro['id'] == id:
+            livros.remove(livro)
+            with open("livrojson.json", "w")as arquivo:
+                json.dump(livros, arquivo, indent=4)
+            # informa ao Flask para nao manter o arquivo .json em cache
+            app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+            return jsonify(livros)
+    return jsonify({'erro': 'livro nao encontrado'})
 
 
 app.run(port=5000, host='localhost', debug=True)
